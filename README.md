9ba842fade6ce1355c4fcb55f1908210d763f68c
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-06-28 01:01:22 -0700

    first basic game

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Hangman-Game
+
+. The computer chooses an animal and displays blanks equal to the number of letters of this animal.
+The user starts with 15 guesses. 
+. The user guesses a letter and enters it.
+.each time the users enters a letter, the guesses decrease by one. 
+. if the user enters a correct letter it is displayed replacing its corresponding blank
+. if the user enters a wrong letter it is added to the letters already guessed. 
+. if you guess a letter more than once, it is not added to the letters already guessed and the number of guesses doesn't decrease because it is already guessed before.
+. if the word is correctrly completed then the picture of the animal is displayed and the sound of the animal is played. And the game is reset.
+. if the number of guesses is zero and the user didn't know the word, the user loses and the game is also reset.
---
+++ assets/javascript/game.js
@@ -0,0 +1,160 @@
+
+
+//variables for the whole game
+var wins = 0;
+var losses = 0;
+var animalsIndex = 0;
+var animals = ["horse", "dog", "cat"];//, "elephant", "lion", "tiger", "zebra"];
+
+// variables for each round. they are reset each round
+// a game round is when displaying a new word
+var guessesLeft = 10;
+var guessedLetters = [];
+var currentAnimal = "";
+var blanks = "";
+
+
+// resetGame is a function called to reset the entire game
+var resetGame = function(){
+
+	wins = 0;
+	losses = 0;
+	animalsIndex = 0;
+	currentAnimal = "";
+	resetRound();
+}
+
+// restRound is a function called to reset the game round
+var resetRound = function(){
+
+	guessesLeft = 10;
+	guessedLetters = [];
+	blanks = "";
+
+	currentAnimal = animals[animalsIndex];
+	animalsIndex++;
+	
+	for(var i=0; i < currentAnimal.length; i++){
+
+		blanks+= "-";
+	}
+	console.log(blanks);
+	document.getElementById("word").innerHTML = blanks;
+	
+}
+
+resetRound();
+
+// replaceLetter function is a function used to replace a letter at a given
+//index in a given string by another given letter.
+
+var replaceLetter = function(str, index, ltr){
+	
+
+	return str.substr(0, index) + ltr + str.substr(index+1);
+	
+}
+
+// updateGessedLetters is a function to udate and display the
+//wrongly guessed letters
+var updateGuessedLetters = function(ltr, arr, match){
+
+	var found = false;
+	arr.forEach(function(element){
+
+			if (ltr === element){
+				found = true;
+			}
+	});
+
+	if(!found && !match){
+
+			arr.push(ltr);
+			guessesLeft--;
+	}
+
+	console.log(arr);
+	document.getElementById("letters-guessed").innerHTML = arr;
+}
+
+// checkCompletion is a function that checks if the current round is complete either
+// when the guesses left reach 0 or when the user guess the word correctly 
+
+var	checkCompletion = function(str1, str2, num){
+
+	if(str1 === str2){
+		wins++;
+		return true;
+	}
+
+	else if(num === 0){
+		losses++;
+		return true;
+	}
+}
+
+
+//main function called with every key stroke
+
+document.onkeypress = function(event){
+
+	if(event.charCode >= 97 && event.charCode <=122 ){
+
+		var userGuess = event.key;
+
+
+		var match = false;
+
+
+		for(var i = 0; i<currentAnimal.length; i++ ){
+
+
+			if( userGuess === currentAnimal.charAt(i)){
+			
+				blanks = replaceLetter(blanks, i, userGuess);
+				match = true;
+			
+			}
+		
+	
+		}
+
+
+		updateGuessedLetters(userGuess, guessedLetters, match);
+
+
+
+		console.log(blanks);
+		document.getElementById("word").innerHTML = blanks;
+
+
+		var complete = checkCompletion (blanks, currentAnimal, guessesLeft);
+
+		if (complete) {
+			console.log("we are done");
+			console.log("Wins: " + wins);
+			document.getElementById("wins").innerHTML = wins;
+
+			console.log("Losses: "+ losses);
+			document.getElementById("losses").innerHTML = losses;
+
+			if (animalsIndex < animals.length){	
+				console.log("It's a new round");
+				resetRound();
+			}	
+			else{
+				console.log("It's a new game");
+				resetGame();
+			}
+
+		}
+
+		document.getElementById("remaining-guesses").innerHTML = guessesLeft; 
+	}
+	else{
+
+		console.log("Please enter a valid letter");
+	}	
+	//console.log(guessedLetters);
+}
+
